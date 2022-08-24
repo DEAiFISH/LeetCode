b92698e4ae7feab247307b0bcf41caac24657a26
DEAIFISH <1277676309@qq.com>
2022-08-24 17:24:47 +0800

    2022/08/24

--- editor/cn/[112]路径总和.java
+++ editor/cn/[112]路径总和.java
@@ -61,8 +61,21 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return helper(root, 0, sum);
     }
+
+    public boolean helper(TreeNode root, int cur, int sum) {
+        if (root == null) {
+            return false;
+        }
+        cur = cur + root.val;
+        if (root.left == null && root.right == null) {
+            return cur == sum;
+        } else {
+            return helper(root.left, cur, sum) || helper(root.right, cur, sum);
+        }
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
