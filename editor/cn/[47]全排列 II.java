9039b1b2a3f3246a8983d56c1d25d3e725b7649b
DEAIFISH <1277676309@qq.com>
2022-08-28 20:57:11 +0800

    2022/08/27

--- editor/cn/[47]全排列 II.java
+++ editor/cn/[47]全排列 II.java
@@ -1,26 +1,22 @@
-//给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。 
+//给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。 
 //
 // 
 //
 // 示例 1： 
 //
 // 
-//输入：nums = [1,2,3]
-//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+//输入：nums = [1,1,2]
+//输出：
+//[[1,1,2],
+// [1,2,1],
+// [2,1,1]]
 // 
 //
 // 示例 2： 
 //
 // 
-//输入：nums = [0,1]
-//输出：[[0,1],[1,0]]
-// 
-//
-// 示例 3： 
-//
-// 
-//输入：nums = [1]
-//输出：[[1]]
+//输入：nums = [1,2,3]
+//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 // 
 //
 // 
@@ -28,17 +24,19 @@
 // 提示： 
 //
 // 
-// 1 <= nums.length <= 6 
+// 1 <= nums.length <= 8 
 // -10 <= nums[i] <= 10 
-// nums 中的所有整数 互不相同 
 // 
 //
-// Related Topics 数组 回溯 👍 2169 👎 0
+// Related Topics 数组 回溯 👍 1174 👎 0
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         int[] vis = new int[nums.length];
         dfs(ans, new ArrayList<Integer>(), nums, vis, 0);
@@ -47,7 +45,10 @@ class Solution {
 
     void dfs(List<List<Integer>> ans, List<Integer> a, int[] nums, int[] vis, int index) {
         if (index == nums.length) {
-            ans.add(new ArrayList<Integer>(a));
+            if (!ans.contains(a)) {
+                ans.add(new ArrayList<Integer>(a));
+
+            }
             return;
         }
         for (int i = 0; i < nums.length; i++) {
