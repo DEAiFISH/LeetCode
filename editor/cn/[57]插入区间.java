0b8e2d3315a76808d3bbd34f9d95a48f5ecd4713
DEAIFISH <1277676309@qq.com>
2022-08-31 13:46:28 +0800

    2022/08/31

---
+++ editor/cn/[57]插入区间.java
@@ -0,0 +1,83 @@
+//给你一个 无重叠的 ，按照区间起始端点排序的区间列表。 
+//
+// 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
+//输出：[[1,5],[6,9]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+//输出：[[1,2],[3,10],[12,16]]
+//解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。 
+//
+// 示例 3： 
+//
+// 
+//输入：intervals = [], newInterval = [5,7]
+//输出：[[5,7]]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：intervals = [[1,5]], newInterval = [2,3]
+//输出：[[1,5]]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：intervals = [[1,5]], newInterval = [2,7]
+//输出：[[1,7]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= intervals.length <= 10⁴ 
+// intervals[i].length == 2 
+// 0 <= intervals[i][0] <= intervals[i][1] <= 10⁵ 
+// intervals 根据 intervals[i][0] 按 升序 排列 
+// newInterval.length == 2 
+// 0 <= newInterval[0] <= newInterval[1] <= 10⁵ 
+// 
+//
+// Related Topics 数组 👍 632 👎 0
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int[][] ints = new int[intervals.length + 1][2];
+
+        System.arraycopy(intervals, 0, ints, 0, intervals.length);
+        ints[ints.length - 1] = newInterval;
+        Arrays.sort(ints, (v1, v2) -> v1[0] - v2[0]);
+
+        int[][] res = new int[ints.length][2];
+        int index = -1;
+
+        for (int[] interval : ints) {
+            if (index == -1 || interval[0] > res[index][1]) {
+                res[++index] = interval;
+            } else {
+                res[index][1] = Math.max(res[index][1], interval[1]);
+            }
+        }
+        return Arrays.copyOf(res, index + 1);
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[59]螺旋矩阵 II.java
@@ -0,0 +1,50 @@
+//给你一个正整数 n ，生成一个包含 1 到 n² 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：n = 3
+//输出：[[1,2,3],[8,9,4],[7,6,5]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：[[1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 20 
+// 
+//
+// Related Topics 数组 矩阵 模拟 👍 804 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        int[][] mat = new int[n][n];
+        int num = 1, tar = n * n;
+        while (num <= tar) {
+            for (int i = l; i <= r; i++) mat[t][i] = num++; // left to right.
+            t++;
+            for (int i = t; i <= b; i++) mat[i][r] = num++; // top to bottom.
+            r--;
+            for (int i = r; i >= l; i--) mat[b][i] = num++; // right to left.
+            b--;
+            for (int i = b; i >= t; i--) mat[i][l] = num++; // bottom to top.
+            l++;
+        }
+        return mat;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
