3ddbe8d1e028eaeb9c071d67f4e92bc35a0c037c
DEAIFISH <1277676309@qq.com>
2022-09-03 15:07:49 +0800

    2022/09/03

---
+++ editor/cn/[206]反转链表.java
@@ -0,0 +1,81 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+//
+// Related Topics 递归 链表 👍 2739 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode res = new ListNode();
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        helper(head);
+        head.next = null;
+        return res;
+    }
+
+    private ListNode helper(ListNode temp) {
+        if (temp.next == null) {
+            res = temp;
+            return temp;
+        }
+
+        helper(temp.next).next = temp;
+
+        return temp;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
