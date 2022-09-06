5bf97fb61a9539838964f501d8d8295b00b636ff
DEAIFISH <1277676309@qq.com>
2022-09-06 11:55:46 +0800

    2022/09/06

---
+++ editor/cn/[77]组合.java
@@ -0,0 +1,68 @@
+//给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。 
+//
+// 你可以按 任何顺序 返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 4, k = 2
+//输出：
+//[
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+//] 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1, k = 1
+//输出：[[1]] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 20 
+// 1 <= k <= n 
+// 
+//
+// Related Topics 回溯 👍 1122 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        combineHelper(n, k, 1);
+        return result;
+    }
+
+    /**
+     * 每次从集合中选取元素，可选择的范围随着选择的进行而收缩，调整可选择的范围，就是要靠startIndex
+     *
+     * @param startIndex 用来记录本层递归的中，集合从哪里开始遍历（集合就是[1,...,n] ）。
+     */
+    private void combineHelper(int n, int k, int startIndex) {
+        //终止条件
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++) {
+            path.add(i);
+            combineHelper(n, k, i + 1);
+            path.removeLast();
+        }
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[78]子集.java
@@ -0,0 +1,60 @@
+//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。 
+//
+// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0]
+//输出：[[],[0]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10 
+// -10 <= nums[i] <= 10 
+// nums 中的所有元素 互不相同 
+// 
+//
+// Related Topics 位运算 数组 回溯 👍 1778 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+
+        //采用回溯法
+        backTrace(nums, res, 0, new ArrayList());
+
+        return res;
+
+    }
+
+    public void backTrace(int[] nums, List<List<Integer>> res, int index, List<Integer> trace) {
+        List<Integer> vals = new ArrayList(trace);
+        res.add(vals);
+
+        //根据示例，组合起来得子集是有序得，故起点为index+1
+        for (int i = index; i < nums.length; i++) {
+            trace.add(nums[i]);
+            backTrace(nums, res, i + 1, trace);
+            trace.remove(trace.size() - 1);
+        }
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
