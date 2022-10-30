1e5359b0dcc23bbbf34f8b335c77472c07778373
DEAIFISH <1277676309@qq.com>
2022-10-30 15:18:53 +0800

    2022/10/30

--- editor/cn/[60]排列序列.java
+++ editor/cn/[60]排列序列.java
@@ -48,10 +48,35 @@
 // Related Topics 递归 数学 👍 703 👎 0
 
 
+import java.util.Arrays;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String getPermutation(int n, int k) {
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            factorial[i] = factorial[i - 1] * i;
+        }
 
+        --k;
+        StringBuffer ans = new StringBuffer();
+        int[] valid = new int[n + 1];
+        Arrays.fill(valid, 1);
+        for (int i = 1; i <= n; ++i) {
+            int order = k / factorial[n - i] + 1;
+            for (int j = 1; j <= n; ++j) {
+                order -= valid[j];
+                if (order == 0) {
+                    ans.append(j);
+                    valid[j] = 0;
+                    break;
+                }
+            }
+            k %= factorial[n - i];
+        }
+        return ans.toString();
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
