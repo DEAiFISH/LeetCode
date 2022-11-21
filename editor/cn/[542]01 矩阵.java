760935170cafb4806716dae5ff4d38b6fbc4351f
DEAIFISH <1277676309@qq.com>
2022-11-21 17:13:45 +0800

    2022/11/21

---
+++ editor/cn/[542]01 矩阵.java
@@ -0,0 +1,79 @@
+//给定一个由 0 和 1 组成的矩阵 mat ，请输出一个大小相同的矩阵，其中每一个格子是 mat 中对应位置元素到最近的 0 的距离。 
+//
+// 两个相邻元素间的距离为 1 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：mat = [[0,0,0],[0,1,0],[0,0,0]]
+//输出：[[0,0,0],[0,1,0],[0,0,0]]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：mat = [[0,0,0],[0,1,0],[1,1,1]]
+//输出：[[0,0,0],[0,1,0],[1,2,1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == mat.length 
+// n == mat[i].length 
+// 1 <= m, n <= 10⁴ 
+// 1 <= m * n <= 10⁴ 
+// mat[i][j] is either 0 or 1. 
+// mat 中至少有一个 0 
+// 
+//
+// Related Topics 广度优先搜索 数组 动态规划 矩阵 👍 780 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dist = new int[m][n];
+        boolean[][] seen = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<int[]>();
+        // 将所有的 0 添加进初始队列中
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                    seen[i][j] = true;
+                }
+            }
+        }
+
+        // 广度优先搜索
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int i = cell[0], j = cell[1];
+            for (int d = 0; d < 4; ++d) {
+                int ni = i + dirs[d][0];
+                int nj = j + dirs[d][1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && !seen[ni][nj]) {
+                    dist[ni][nj] = dist[i][j] + 1;
+                    queue.offer(new int[]{ni, nj});
+                    seen[ni][nj] = true;
+                }
+            }
+        }
+
+        return dist;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
