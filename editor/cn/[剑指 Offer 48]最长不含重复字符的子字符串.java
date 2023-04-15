dc54eb8b3703ac496e912ed348ed71b05f1a0068
DEAIFISH <1277676309@qq.com>
2023-04-15 22:03:07 +0800

    2023/04/15

---
+++ editor/cn/[剑指 Offer 48]最长不含重复字符的子字符串.java
@@ -0,0 +1,66 @@
+//请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 输入: "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 输入: "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// s.length <= 40000 
+// 
+//
+// 注意：本题与主站 3 题相同：https://leetcode-cn.com/problems/longest-substring-without-
+//repeating-characters/ 
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 576 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
