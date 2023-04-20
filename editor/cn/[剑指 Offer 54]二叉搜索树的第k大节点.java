2a2ee82832492fb6c32ec67b7298c8833bd85ca4
DEAIFISH <1277676309@qq.com>
2023-04-20 11:32:40 +0800

    2023/04/20

---
+++ editor/cn/[剑指 Offer 54]二叉搜索树的第k大节点.java
@@ -0,0 +1,69 @@
+//给定一棵二叉搜索树，请找出其中第 k 大的节点的值。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: root = [3,1,4,null,2], k = 1
+//   3
+//  / \
+// 1   4
+//  \
+//   2
+//输出: 4 
+//
+// 示例 2: 
+//
+// 
+//输入: root = [5,3,6,2,4,null,null,1], k = 3
+//       5
+//      / \
+//     3   6
+//    / \
+//   2   4
+//  /
+// 1
+//输出: 4 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 ≤ k ≤ 二叉搜索树元素个数 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 381 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int res, k;
+
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.right);
+        if (k == 0) return;
+        if (--k == 0) res = root.val;
+        dfs(root.left);
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
