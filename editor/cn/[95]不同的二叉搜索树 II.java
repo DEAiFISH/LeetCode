f7f47799a5fe2e8ac5e80954f3c13ca04a5cb651
DEAIFISH <1277676309@qq.com>
2023-04-24 09:33:04 +0800

    2023/04/24

---
+++ editor/cn/[95]不同的二叉搜索树 II.java
@@ -0,0 +1,56 @@
+//给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。 
+//
+// 
+//
+// 
+// 
+// 示例 1： 
+// 
+// 
+//输入：n = 3
+//输出：[[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+// 
+// 
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：[[1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 8 
+// 
+//
+// Related Topics 树 二叉搜索树 动态规划 回溯 二叉树 👍 1417 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
