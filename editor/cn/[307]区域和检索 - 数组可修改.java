d5820cec64f5558f9efd7d2fde8ef03fb6596d96
DEAIFISH <1277676309@qq.com>
2023-06-21 23:13:00 +0800

    2023/06/21

---
+++ editor/cn/[307]区域和检索 - 数组可修改.java
@@ -0,0 +1,100 @@
+//给你一个数组 nums ，请你完成两类查询。 
+//
+// 
+// 其中一类查询要求 更新 数组 nums 下标对应的值 
+// 另一类查询要求返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元素的 和 ，其中 left <= right 
+// 
+//
+// 实现 NumArray 类： 
+//
+// 
+// NumArray(int[] nums) 用整数数组 nums 初始化对象 
+// void update(int index, int val) 将 nums[index] 的值 更新 为 val 
+// int sumRange(int left, int right) 返回数组 nums 中索引 left 和索引 right 之间（ 包含 ）的nums元
+//素的 和 （即，nums[left] + nums[left + 1], ..., nums[right]） 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：
+//["NumArray", "sumRange", "update", "sumRange"]
+//[[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+//输出：
+//[null, 9, null, 8]
+//
+//解释：
+//NumArray numArray = new NumArray([1, 3, 5]);
+//numArray.sumRange(0, 2); // 返回 1 + 3 + 5 = 9
+//numArray.update(1, 2);   // nums = [1,2,5]
+//numArray.sumRange(0, 2); // 返回 1 + 2 + 5 = 8
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// -100 <= nums[i] <= 100 
+// 0 <= index < nums.length 
+// -100 <= val <= 100 
+// 0 <= left <= right < nums.length 
+// 调用 update 和 sumRange 方法次数不大于 3 * 10⁴ 
+// 
+//
+// Related Topics 设计 树状数组 线段树 数组 
+// 👍 613 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class NumArray {
+    private int[] tree;
+    private int[] nums;
+
+    public NumArray(int[] nums) {
+        this.tree = new int[nums.length + 1];
+        this.nums = nums;
+        for (int i = 0; i < nums.length; i++) {
+            add(i + 1, nums[i]);
+        }
+    }
+
+    public void update(int index, int val) {
+        add(index + 1, val - nums[index]);
+        nums[index] = val;
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum(right + 1) - prefixSum(left);
+    }
+
+    private int lowBit(int x) {
+        return x & -x;
+    }
+
+    private void add(int index, int val) {
+        while (index < tree.length) {
+            tree[index] += val;
+            index += lowBit(index);
+        }
+    }
+
+    private int prefixSum(int index) {
+        int sum = 0;
+        while (index > 0) {
+            sum += tree[index];
+            index -= lowBit(index);
+        }
+        return sum;
+    }
+}
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
--- editor/cn/[685]冗余连接 II.java
+++ editor/cn/[685]冗余连接 II.java
@@ -42,33 +42,64 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    int[] father;
-
     public int[] findRedundantDirectedConnection(int[][] edges) {
         int n = edges.length;
-        father = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            father[i] = i;
+        UnionFind uf = new UnionFind(n + 1);
+        int[] parent = new int[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            parent[i] = i;
         }
-
-        for (int i = 0; i < n; i++) {
-            if (find(edges[i][0]) == find(edges[i][1])) {
-                return edges[i];
+        int conflict = -1;
+        int cycle = -1;
+        for (int i = 0; i < n; ++i) {
+            int[] edge = edges[i];
+            int node1 = edge[0], node2 = edge[1];
+            if (parent[node2] != node2) {
+                conflict = i;
+            } else {
+                parent[node2] = node1;
+                if (uf.find(node1) == uf.find(node2)) {
+                    cycle = i;
+                } else {
+                    uf.union(node1, node2);
+                }
             }
-            union(edges[i][0], edges[i][1]);
         }
-        return null;
+        if (conflict < 0) {
+            int[] redundant = {edges[cycle][0], edges[cycle][1]};
+            return redundant;
+        } else {
+            int[] conflictEdge = edges[conflict];
+            if (cycle >= 0) {
+                int[] redundant = {parent[conflictEdge[1]], conflictEdge[1]};
+                return redundant;
+            } else {
+                int[] redundant = {conflictEdge[0], conflictEdge[1]};
+                return redundant;
+            }
+        }
+    }
+}
+
+class UnionFind {
+    int[] ancestor;
+
+    public UnionFind(int n) {
+        ancestor = new int[n];
+        for (int i = 0; i < n; ++i) {
+            ancestor[i] = i;
+        }
     }
 
-    private void union(int i, int j) {
-        father[find(j)] = find(i);
+    public void union(int index1, int index2) {
+        ancestor[find(index1)] = find(index2);
     }
 
-    private int find(int x) {
-        if (x != father[x]) {
-            father[x] = find(father[x]);
+    public int find(int index) {
+        if (ancestor[index] != index) {
+            ancestor[index] = find(ancestor[index]);
         }
-        return father[x];
+        return ancestor[index];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
