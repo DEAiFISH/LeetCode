49e6bd98b577d2d25091e3ee3eebe13e61a3d77f
DEAIFISH <1277676309@qq.com>
2023-07-29 22:03:14 +0800

    2023/07/29

--- editor/cn/[11]盛最多水的容器.java
+++ editor/cn/[11]盛最多水的容器.java
@@ -40,9 +40,9 @@
 class Solution {
     public int maxArea(int[] height) {
         int l = 0, r = height.length - 1;
-        int maxArea = 0, area = 0;
+        int maxArea = 0;
         while (l < r) {
-            maxArea = Math.max(maxArea, (r - l) * ((height[l] <= height[r]) ? height[l] : height[r]));
+            maxArea = Math.max(maxArea, (r - l) * (Math.min(height[l], height[r])));
             if (height[l] < height[r]) {
                 l++;
             } else {
--- editor/cn/[15]三数之和.java
+++ editor/cn/[15]三数之和.java
@@ -42,7 +42,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList();
         int len = nums.length;
-        if (nums == null || len < 3) return ans;
+        if (len < 3) return ans;
         Arrays.sort(nums); // 排序
         for (int i = 0; i < len; i++) {
             if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
@@ -58,7 +58,7 @@ class Solution {
                     L++;
                     R--;
                 } else if (sum < 0) L++;
-                else if (sum > 0) R--;
+                else R--;
             }
         }
         return ans;
