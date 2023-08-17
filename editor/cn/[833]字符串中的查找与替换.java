84c0e3ad6deaf5589475be4e05cde0a495095069
DEAIFISH <1277676309@qq.com>
2023-08-17 17:20:05 +0800

    2023/08/17

--- editor/cn/[833]字符串中的查找与替换.java
+++ editor/cn/[833]字符串中的查找与替换.java
@@ -70,7 +70,37 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        int n = s.length(), m = indices.length;
 
+        List<Integer> ops = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            ops.add(i);
+        }
+        ops.sort((i, j) -> indices[i] - indices[j]);
+
+        StringBuilder ans = new StringBuilder();
+        int pt = 0;
+        for (int i = 0; i < n; ) {
+            while (pt < m && indices[ops.get(pt)] < i) {
+                pt++;
+            }
+            boolean succeed = false;
+            while (pt < m && indices[ops.get(pt)] == i) {
+                if (s.substring(i, Math.min(i + sources[ops.get(pt)].length(), n)).equals(sources[ops.get(pt)])) {
+                    succeed = true;
+                    break;
+                }
+                pt++;
+            }
+            if (succeed) {
+                ans.append(targets[ops.get(pt)]);
+                i += sources[ops.get(pt)].length();
+            } else {
+                ans.append(s.charAt(i));
+                i++;
+            }
+        }
+        return ans.toString();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
