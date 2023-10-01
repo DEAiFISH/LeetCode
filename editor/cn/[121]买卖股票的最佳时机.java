12443a0d4b915ef7528aed0f79a47190e68671e0
DEAIFISH <1277676309@qq.com>
2023-10-01 23:22:17 +0800

    2023/10/01

--- editor/cn/[121]买卖股票的最佳时机.java
+++ editor/cn/[121]买卖股票的最佳时机.java
@@ -40,6 +40,7 @@ public class Solution {
     public int maxProfit(int prices[]) {
         int minprice = Integer.MAX_VALUE;
         int maxprofit = 0;
+
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minprice) {
                 minprice = prices[i];
