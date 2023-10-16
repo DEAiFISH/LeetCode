7af83a067d7c53b61e1ebd08a6deb6c45e796050
DEAIFISH <1277676309@qq.com>
2023-10-16 21:17:36 +0800

    2023/10/16

---
+++ editor/cn/[260]只出现一次的数字 III.java
@@ -0,0 +1,65 @@
+//给你一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按 任意顺序 返回答案。 
+//
+// 你必须设计并实现线性时间复杂度的算法且仅使用常量额外空间来解决此问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,1,3,2,5]
+//输出：[3,5]
+//解释：[5, 3] 也是有效的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [-1,0]
+//输出：[-1,0]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0,1]
+//输出：[1,0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 3 * 10⁴ 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// 除两个只出现一次的整数外，nums 中的其他数字都出现两次 
+// 
+//
+// Related Topics 位运算 数组 
+// 👍 816 👎 0
+
+
+import java.util.HashSet;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+
+        int[] res = new int[set.size()];
+        int i = 0;
+        for (int num : set) {
+            res[i++] = num;
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
