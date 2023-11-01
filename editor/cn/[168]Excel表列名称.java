65c2566f5b021fb1726e7b118fce95942ba533ca
DEAiFISH <DEAiFISH@Outlook.com>
2023-11-01 17:27:50 +0800

    2023/11/01

---
+++ editor/cn/[168]Excel表列名称.java
@@ -0,0 +1,71 @@
+//给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。 
+//
+// 例如： 
+//
+// 
+//A -> 1
+//B -> 2
+//C -> 3
+//...
+//Z -> 26
+//AA -> 27
+//AB -> 28 
+//...
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：columnNumber = 1
+//输出："A"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：columnNumber = 28
+//输出："AB"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：columnNumber = 701
+//输出："ZY"
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：columnNumber = 2147483647
+//输出："FXSHRXW"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= columnNumber <= 2³¹ - 1 
+// 
+//
+// Related Topics 数学 字符串 
+// 👍 658 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuffer sb = new StringBuffer();
+        while (columnNumber > 0) {
+            int a0 = (columnNumber - 1) % 26 + 1;
+            sb.append((char) (a0 - 1 + 'A'));
+            columnNumber = (columnNumber - a0) / 26;
+        }
+        return sb.reverse().toString();
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
