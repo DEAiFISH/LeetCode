cd67aa24acfe46f8895875c9834639bc8d92ea02
DEAiFISH <DEAiFISH@Outlook.com>
2023-11-10 15:04:20 +0800

    2023/11/10

--- editor/cn/[2300]咒语和药水的成功对数.java
+++ editor/cn/[2300]咒语和药水的成功对数.java
@@ -45,10 +45,34 @@
 // 👍 64 👎 0
 
 
+import java.util.Arrays;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length, m = potions.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            long t = (success + spells[i] - 1) / spells[i] - 1;
+            res[i] = m - binarySearch(potions, 0, m - 1, t);
+        }
+        return res;
+    }
 
+    public int binarySearch(int[] arr, int lo, int hi, long target) {
+        int res = hi + 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] > target) {
+                res = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return res;
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
