c7c6294df1919677c89df3004b472b25bd3d5e8b
大鹅爱吃酸菜鱼 <DEAiFISH@Outlook.com>
2024-02-22 14:39:40 +0800

    2024/02/22

---
+++ editor/cn/[889]根据前序和后序遍历构造二叉树.java
@@ -0,0 +1,85 @@
+//给定两个整数数组，preorder 和 postorder ，其中 preorder 是一个具有 无重复 值的二叉树的前序遍历，postorder 是同一棵
+//树的后序遍历，重构并返回二叉树。 
+//
+// 如果存在多个答案，您可以返回其中 任何 一个。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+//输出：[1,2,3,4,5,6,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [1], postorder = [1]
+//输出: [1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= preorder.length <= 30 
+// 1 <= preorder[i] <= preorder.length 
+// preorder 中所有值都 不同 
+// postorder.length == preorder.length 
+// 1 <= postorder[i] <= postorder.length 
+// postorder 中所有值都 不同 
+// 保证 preorder 和 postorder 是同一棵二叉树的前序遍历和后序遍历 
+// 
+//
+// Related Topics 树 数组 哈希表 分治 二叉树 
+// 👍 351 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        int n = preorder.length;
+        Map<Integer, Integer> postMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            postMap.put(postorder[i], i);
+        }
+        return dfs(preorder, postorder, postMap, 0, n - 1, 0, n - 1);
+    }
+
+    public TreeNode dfs(int[] preorder, int[] postorder, Map<Integer, Integer> postMap, int preLeft, int preRight, int postLeft, int postRight) {
+        if (preLeft > preRight) {
+            return null;
+        }
+        int leftCount = 0;
+        if (preLeft < preRight) {
+            leftCount = postMap.get(preorder[preLeft + 1]) - postLeft + 1;
+        }
+        return new TreeNode(preorder[preLeft],
+                dfs(preorder, postorder, postMap, preLeft + 1, preLeft + leftCount, postLeft, postLeft + leftCount - 1),
+                dfs(preorder, postorder, postMap, preLeft + leftCount + 1, preRight, postLeft + leftCount, postRight - 1));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
