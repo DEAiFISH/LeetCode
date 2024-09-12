758d3b8fe85aaf3710916c8bca3fa4f6686258ce
大鹅爱吃酸菜鱼 <deaifish@outlook.com>
2024-09-12 14:53:24 +0800

    -

---
+++ leetcode/editor/cn/[264]丑数 II.java
@@ -0,0 +1,62 @@
+//给你一个整数 n ，请你找出并返回第 n 个 丑数 。 
+//
+// 丑数 就是质因子只包含 2、3 和 5 的正整数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 10
+//输出：12
+//解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：1
+//解释：1 通常被视为丑数。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 1690 
+// 
+//
+// Related Topics 哈希表 数学 动态规划 堆（优先队列） 
+// 👍 1215 👎 0
+
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] factors = {2, 3, 5};
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
